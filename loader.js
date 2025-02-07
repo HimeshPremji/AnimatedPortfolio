96dffb4654fc9fe79f01a186dd7e70bc2b651b01
HimeshPremji <himeshdmaheswary@gmail.com>
2025-02-08 00:40:36 +0500

    Navbar animations finished

--- loader.js
+++ loader.js
@@ -2,20 +2,3 @@ const clips = document.querySelectorAll(".clips");
 window.addEventListener("load", () => {
   clips.style.height = "100%";
 });
-// gsap.fromTo(
-//   clips,
-//   {
-//     height: "100%",
-//   },
-//   {
-//     delay: 0.1,
-//     height: 0,
-//     transformOrigin: "top",
-//     stagger: 0.06,
-//     duration: 1.4,
-//     ease: "power4.inOut",
-//     onComplete: () => {
-//       gsap.to(".loader", { autoAlpha: 0 });
-//     },
-//   }
-// );
--- script.js
+++ script.js
@@ -1,19 +1,57 @@
 // Navbar Script
-document.querySelector(".hamburger").addEventListener("click", () => {
-  const navLinks = document.querySelector(".nav-links");
-  navLinks.classList.toggle("active");
-});
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navbarBg = document.querySelector(".navbar-bg");
+let menuOpen = false;
 
-// nav-links and button gsap animation
-// create gsap timeline///
+// Hamburger Menu Animation
+hamburger.addEventListener("click", () => {
+  menuOpen = !menuOpen;
+
+  if (menuOpen) {
+    navLinks.classList.add("active");
+    gsap.fromTo(
+      navLinks,
+      { height: 0, opacity: 0, display: "none" },
+      {
+        height: "auto",
+        opacity: 1,
+        display: "flex",
+        duration: 0.5,
+        ease: "power4.inOut",
+      }
+    );
+
+    document.querySelectorAll(".nav-links li").forEach((link) => {
+      link.addEventListener("click", () => {
+        gsap.to(navLinks, {
+          height: 0,
+          opacity: 0,
+          duration: 0.5,
+          ease: "power4.inOut",
+          onComplete: () => {
+            navLinks.classList.remove("active");
+            menuOpen = false;
+          },
+        });
+      });
+    });
+  } else {
+    gsap.to(navLinks, {
+      height: 0,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power4.inOut",
+      onComplete: () => navLinks.classList.remove("active"),
+    });
+  }
+});
+// Timeline
 const tl = gsap.timeline();
 tl.fromTo(
   ".clips",
+  { height: "100%" },
   {
-    height: "100%",
-  },
-  {
-    delay: 0.1,
     height: 0,
     transformOrigin: "top",
     stagger: 0.06,
@@ -21,21 +59,35 @@ tl.fromTo(
     ease: "power4.inOut",
   }
 )
-  .to(".navbar-bg", {
-    width: "101%",
-    background: "white",
-    duration: 1,
-    ease: "power2.inOut",
-  }, "-=0.8")
-  .from(".nav-links li", {
-    opacity: 0,
-    x: -50,
-    duration: 1,
-    ease: "power1.inOut",
-  }, "-=1")
-  .from(".navbar .resume", {
-    opacity: 0,
-    duration: .8,
-    ease: "power1.inOut",
-  }, "-=.8")
-  .to(".loader", { autoAlpha: 0 }, "-=.5");
+  .to(
+    navbarBg,
+    {
+      width: "101%",
+      background: "white",
+      duration: 1,
+      ease: "power2.inOut",
+    },
+    "-=0.8"
+  )
+  .from(
+    ".nav-links li",
+    {
+      opacity: 0,
+      x: -50,
+      duration: 1,
+      ease: "power1.inOut",
+    },
+    "-=1"
+  )
+  .from(
+    ".navbar .resume",
+    {
+      opacity: 0,
+      duration: 0.8,
+      ease: "power1.inOut",
+    },
+    "-=0.8"
+  )
+  .to(".loader", { autoAlpha: 0 }, "-=0.5");
+
+// Navbar Animation Ends Here
